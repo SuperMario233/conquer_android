76a4ea260f66766f723b3c44e53f3fd7486d1849
XiaoZheng <1500012822@pku.edu.cn>
2018-12-13 02:19:51 +0800

    实现了邮件验证码功能&初步设计了个人主页

--- app/src/main/java/com/binarypheasant/conquer/sign_up.java
+++ app/src/main/java/com/binarypheasant/conquer/sign_up.java
@@ -19,10 +19,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class sign_up extends AppCompatActivity {
 
-    String account,password,group,nickname,statusCode;
+    String account,password,group,nickname,statusCode,token,checkCode,errorCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +41,12 @@ public class sign_up extends AppCompatActivity {
                 EditText user_passwordText = findViewById(R.id.passwordText);
                 EditText user_group = findViewById(R.id.teamText);
                 EditText user_nickname = findViewById(R.id.nicknameText);
+                EditText user_checkcode = findViewById(R.id.checkcodeText);
                 account = user_emailText.getText().toString();
                 password = user_passwordText.getText().toString();
                 group = user_group.getText().toString();
                 nickname = user_nickname.getText().toString();
+                checkCode = user_checkcode.getText().toString();
                 if(TextUtils.isEmpty(account)){
                     user_emailText.setError("用户名不能为空");
                     return;
@@ -60,9 +64,88 @@ public class sign_up extends AppCompatActivity {
                     user_nickname.setError("昵称不能为空");
                     return;
                 }
+                if(TextUtils.isEmpty(checkCode)){
+                    user_checkcode.setError("验证码不能为空");
+                }
                 sendRequestWithHttpURLConnection();
             }
         });
+
+        Button checkButton = findViewById(R.id.checkcodeButton);
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText accountText = findViewById(R.id.accountText);
+                if(isAccount(accountText.getText().toString())){
+                    sendRequestWithHttpURLConnection_check(accountText.getText().toString());
+                }
+                else{
+                    accountText.setError("请输入正确账号");
+                }
+            }
+        });
+    }
+
+    private boolean isAccount(String account){
+        Pattern pattern = Pattern.compile("[0-9]*");
+        Matcher isNum = pattern.matcher(account);
+        return isNum.matches();
+    }
+
+    private void sendRequestWithHttpURLConnection_check(final String account){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try{
+                    URL url = new URL("https://ingfo.huyunfan.cn/user/mail.php");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+                    out.writeBytes("account="+account);
+                    InputStream in = connection.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while((line = reader.readLine()) != null){
+                        response.append(line);
+                    }
+                    //Log.v("HTTPResponse",response.toString());
+
+                    JSONObject jsonresponse = new JSONObject(response.toString());
+                    MailCheck(jsonresponse);
+                    //showResponse(response.toString());
+                } catch (Exception e){
+                    e.printStackTrace();
+                } finally {
+                    if (reader != null){
+                        try{
+                            reader.close();
+                        } catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                    if (connection != null){
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void MailCheck(JSONObject response){
+        try{
+            token = response.getString("token");
+            statusCode = response.getString("response");
+            errorCode = response.getString("ErrorCode");
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        if(statusCode != null) showResponse("验证码发送成功");
+        else showResponse("发送验证码错误："+errorCode);
     }
 
     private void sendRequestWithHttpURLConnection(){
@@ -78,7 +161,7 @@ public class sign_up extends AppCompatActivity {
                     connection.setConnectTimeout(8000);
                     connection.setReadTimeout(8000);
                     DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-                    out.writeBytes("account="+account+"&password="+password+"&group="+group+"&nickname="+nickname);
+                    out.writeBytes("token="+token+"&vocde="+checkCode+"&account="+account+"&password="+password+"&group="+group+"&nickname="+nickname);
                     InputStream in = connection.getInputStream();
                     reader = new BufferedReader(new InputStreamReader(in));
                     StringBuilder response = new StringBuilder();
@@ -125,16 +208,18 @@ public class sign_up extends AppCompatActivity {
         }catch (JSONException e){
             e.printStackTrace();
         }
-        //Toast.makeText(log_in.this, "statusCode:"+statusCode, Toast.LENGTH_LONG).show();
-        //SendRet = false;
 
         if(statusCode.equals("1")) showResponse("用户已存在");
         else if (statusCode.equals("0")){
             showResponse("注册成功");
+            statusCode = null;
             Intent GotoNext = new Intent(sign_up.this, log_in.class);
             startActivity(GotoNext);
         }
-        else showResponse("其他错误，返回值"+String.valueOf(statusCode));
+        else{
+            showResponse("其他错误，返回值"+String.valueOf(statusCode));
+            statusCode = null;
+        }
     }
 
 }
